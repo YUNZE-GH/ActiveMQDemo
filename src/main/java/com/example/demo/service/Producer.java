66d749048f44e04c0ae555d85152de417bef30c0
gaoh <834363368@qq.com>
2020-08-06 13:48:02 +0800

    init

--- src/main/java/com/example/demo/service/Producer.java
+++ src/main/java/com/example/demo/service/Producer.java
@@ -18,7 +18,7 @@ public class Producer {
     @Autowired // 也可以注入JmsTemplate，JmsMessagingTemplate对JmsTemplate进行了封装
     private JmsMessagingTemplate jmsTemplate;
 
-    // 发送消息，destination是发送到的队列，message是待发送的消息
+    // 发送消息，destination是发送到队列的，message是待发送的消息
     public void sendMessage(String name, String phone) {
         Emp emp = new Emp();
         emp.setPhone(phone);
